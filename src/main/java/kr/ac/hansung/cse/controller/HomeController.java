e6014296881c0f3ce655ac8f955eaed773f87b1f
yechanpark <qkrdpcks0419@naver.com>
2017-05-28 17:37:52 +0900

    Logging

--- src/main/java/kr/ac/hansung/cse/controller/HomeController.java
+++ src/main/java/kr/ac/hansung/cse/controller/HomeController.java
@@ -1,21 +1,38 @@
 package kr.ac.hansung.cse.controller;
 
-import java.util.Locale;
+import javax.servlet.http.HttpServletRequest;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-/**
- * Handles requests for the application home page.
- */
 @Controller
 public class HomeController {
 	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
-
+	public String home(HttpServletRequest request) {
+		
+		Logger logger = LoggerFactory.getLogger(HomeController.class);
+		// 패키지명을 포함하여 인자를 넘겨도 상관 없음. 이 방법은 다소 불편
+		// Logger logger = LoggerFactory.getLogger(kr.ac.hansung.cse.controller.HomeController);
+		
+		/*logger.trace("trace level: hello");
+		logger.debug("debug level: hello");
+		logger.info("info level: hello");
+		logger.warn("warn level: hello");
+		logger.error("error level: hello");*/
+		
+		// Request URL,  getRequestURI()로 하지 않도록 주의
+		String url = request.getRequestURL().toString();
+		
+		// client IP Address
+		String clientIPaddress = request.getRemoteAddr();
+		
+		logger.info("request url:" + url);
+		logger.info("client up:" + clientIPaddress);
+		
 		return "home";
 	}
 	
