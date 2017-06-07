27f8203f6cfae836d7e346ba909708bbb24c0612
yechan <yechan@115.21.3.201>
2017-06-08 02:41:32 +0900

    comment 추가 및 들여쓰기

--- src/main/java/kr/ac/hansung/cse/controller/HomeController.java
+++ src/main/java/kr/ac/hansung/cse/controller/HomeController.java
@@ -10,30 +10,37 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class HomeController {
-	
+
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(HttpServletRequest request) {
-		
+
 		Logger logger = LoggerFactory.getLogger(HomeController.class);
+
 		// 패키지명을 포함하여 인자를 넘겨도 상관 없음. 이 방법은 다소 불편
 		// Logger logger = LoggerFactory.getLogger(kr.ac.hansung.cse.controller.HomeController);
-		
-		/*logger.trace("trace level: hello");
-		logger.debug("debug level: hello");
-		logger.info("info level: hello");
-		logger.warn("warn level: hello");
-		logger.error("error level: hello");*/
-		
-		// Request URL,  getRequestURI()로 하지 않도록 주의
+
+		/*
+		 * 이 로거는 "kr.ac.hansung.cse"라는 이름 때문에 logback.xml에 기술돼있는
+		 * kr.ac.hansung.cse logger의 자식이 되며, 따라서 상속받는다. 물론 HomeController.class로
+		 * 기술하여도 상속을 받는다.
+		 */
+
+		// logger.trace("trace level: hello");
+		// logger.debug("debug level: hello");
+		// logger.info("info level: hello");
+		// logger.warn("warn level: hello");
+		// logger.error("error level: hello");
+
+		// Request URL, getRequestURI()로 하지 않도록 주의
 		String url = request.getRequestURL().toString();
-		
+
 		// client IP Address
 		String clientIPaddress = request.getRemoteAddr();
-		
+
 		logger.info("request url:" + url);
 		logger.info("client up:" + clientIPaddress);
-		
+
 		return "home";
 	}
-	
+
 }
